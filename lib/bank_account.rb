3b852008e8abd9a3a787dafb18a5ffc11ecc6006
hellonathanchung <chung.nathanh@gmail.com>
2020-07-17 16:03:00 -0700

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,38 @@
+require 'pry'
 class BankAccount
+    @@all = []
 
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    def initialize(name, balance=1000, status='open')
+        @name = name
+        @balance = balance
+        @status = status
+        @@all << self
+    end
+
+    def deposit(amount)
+        self.balance = self.balance += amount
+    end
+
+    def display_balance
+        return "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        if self.balance > 0 && status == "open"
+            true
+        else 
+            false
+        end
+     end
+
+     def close_account
+        self.status = "closed"
+     end
+
+     def self.all
+        @@all
+     end
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,42 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+  attr_reader :name
+
+  def initialize(sender, receiver, amount)
+  @sender = sender
+  @receiver = receiver
+  @status = "pending"
+  @amount = amount
+  end
+
+  def valid?
+    self.sender.valid? && self.receiver.valid?
+  end
+
+  def execute_transaction
+    if valid? && sender.balance > self.amount && self.status == "pending"
+      # binding.pry
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      return "Transaction rejected. Please check your account balance." 
+    end
+  end
+
+  def reverse_transfer
+    if self.status == "complete"
+      sender.balance += amount
+      receiver.balance -= amount 
+      self.status = "reversed"
+    end
+  end
 end
+ 
+# binding.pry
+# nathan_to_darius = Transfer.new("nathan", "darius", 500) 
+
+# darius_to_nathan = Transfer.new("darius", "nathan", 1000000)
+
+# binding.pry
